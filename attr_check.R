6b6d5f17c337815e170bee1f4f9bb99bf69db7c5
Deja vu <dejavu@Dejas-MacBook-Air.local>
2022-06-23 18:23:15 -0400

    123

--- attr_check.R
+++ attr_check.R
@@ -5,7 +5,7 @@ attr_check <- function(d_new, d_old){
   attr_checklist$cols_match <-  length(setdiff(colnames(d_old), colnames(d_new))) == 0 & ncol(d_old) == ncol(d_new)
   attr_checklist$attrnames_match <- length(setdiff(attr_names$old, attr_names$new)) == 0 &
     length(attr_names$old) == length(attr_names$new)
-  
+  ###testing
   attr_checklist <- as.data.frame(attr_checklist)
   return(attr_checklist)
 }
